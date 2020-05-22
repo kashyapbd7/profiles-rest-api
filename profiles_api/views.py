c6efc89b5b4e2ca2440b3f5c7a7312c112e89906
Kashyap <kashyapbd7@gmail.com>
2020-05-22 18:27:40 -0500

    Added POST, PUT, PATCH, DELETE methods for testing in APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
+
 
 class HelloApiView(APIView):
     """Testing API View"""
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format = None):
         """Return a List of APIView features"""
@@ -15,5 +21,34 @@ class HelloApiView(APIView):
         '5. DELETE',
         ]
 
-        return Response({'message': 'Hello World I am Learning Django', 
+        return Response({'message': 'Hello World I am Learning Django',
                         'an_apiView': an_apiView})
+
+
+    def post(self, request):
+        """Testing POST API"""
+
+        serializer = self.serializer_class (data = request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """Testing the patch"""
+        return Response({'method':'PUT'})
+
+    def patch(self, request):
+        """Testing the patch"""
+        return Response({'method':'PATCH'})
+
+    def delete(self, request):
+        """Testing the patch"""
+        return Response({'method':'DELETE'})
