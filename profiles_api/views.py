559aac08332e23a39dda7faaaf0c02ecf0e49cc2
Kashyap <kashyapbd7@gmail.com>
2020-05-22 19:17:19 -0500

    Added create, retrive, update, partial_update, destroy methods for testing in ViewSet

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
@@ -52,3 +53,52 @@ class HelloApiView(APIView):
     def delete(self, request):
         """Testing the patch"""
         return Response({'method':'DELETE'})
+
+
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Testing ViewSet"""
+
+    serializer_class = serializers.HelloSerializer
+
+
+    def list(self, request):
+        """Creating the List function"""
+        an_viewSet = ['HTTP function is used to get functionalities like ',
+        '1. LIST',
+        '2. CREATE',
+        '3. RETRIVE',
+        '4. UPDATE',
+        '5. PARTIAL UPDATE',]
+
+        return Response({'message': 'Hello I am Learning Viewset', 'an_viewSet':an_viewSet})
+
+    def create(self, request):
+        """"Creating the object"""
+
+        serializer = self.serializer_class(data = request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Retriving the Object"""
+        return Response({'http_method':'GET'})
+
+    def update(self, request, pk=None):
+        """Updating the object"""
+        return Response({'http_method':'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Updating the Object partially"""
+        return Response({'http_method':'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Deleting the object"""
+        return Response({'http_method':'DELETE'})
